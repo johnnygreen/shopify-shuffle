fa47dcf9e23198aefcae28c0b31f5e79a55d0f12
Johnny Green <johnnygreen@gmail.com>
2017-04-29 23:11:01 -0400

    update loaded file location

--- load.js
+++ load.js
@@ -1,6 +1,6 @@
 (function() {
   var elem = document.createElement('script');
-  elem.src = 'fill this in later';
+  elem.src = 'https://raw.githubusercontent.com/johnnygreen/shopify_shuffle/master/shuffle.js';
   elem.type = "text/javascript";
   var script = document.getElementsByTagName('script')[0];
   script.parentNode.insertBefore(elem, script);
